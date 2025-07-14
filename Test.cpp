4cf4dd6b914a0a3bcbad3a6a1ac5d4c93a10ebcf
Роман Попов <rapopov2004@yandex.ru>
2025-07-15 00:40:49 +0300

    gtest added

--- Test.cpp
+++ Test.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "gtest/gtest.h"
 
 using namespace std;
 
-int main()
+TEST(SampleTests, Test1) {
+    EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char **argv)
 {
-    cout << "Hi" << endl;
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
 }
