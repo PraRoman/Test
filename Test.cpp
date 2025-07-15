56f39959f1dbc6de225af8e8d09de6465aa219dc
Роман Попов <rapopov2004@yandex.ru>
2025-07-15 17:48:07 +0300

    new class added

--- Test.cpp
+++ Test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "gtest/gtest.h"
+#include <string>
 
 using namespace std;
 
@@ -7,8 +8,45 @@ TEST(SampleTests, Test1) {
     EXPECT_EQ(1, 1);
 }
 
-int main(int argc, char **argv)
-{
+class BankAccount {
+private:
+    double balance;
+    std::string owner;
+
+public:
+    BankAccount(const std::string& ownerName, double initialBalance)
+        : owner(ownerName), balance(initialBalance) {
+    }
+
+    void deposit(int addition) {
+        if (addition > 0) balance += addition;
+    }
+
+    void withdraw(int reduction) {
+        if (reduction > 0) balance -= reduction;  
+    }
+
+    double getBalance() const {
+        return balance;
+    }
+
+    std::string getOwner() const {
+        return owner;
+    }
+
+    bool hasNonNegativeBalance() const {
+        return balance >= 0;
+    }
+};
+
+int main(int argc, char **argv) {
+    
+    BankAccount person("Tom", 35000);
+    person.getInfo();
+    person.withdraw(10000);
+    person.deposit(150000);
+    person.getInfo();
+    
     ::testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();
